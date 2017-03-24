605ab817971a8c920a96ecacaeb180a683c9f8cf
dq <dengqiang@cqebd.cn>
2017-03-24 13:41:00 +0800

    commit rxbus2.0 by rxjava2.0

---
+++ rxbus/src/main/java/gorden/rxbus2/ThreadMode.java
@@ -0,0 +1,23 @@
+package gorden.rxbus2;
+
+/**
+ *
+ * Created by gorden on 2016/7/23.
+ */
+public enum ThreadMode {
+    /**
+     * current thread
+     */
+    CURRENT_THREAD,
+
+    /**
+     * android main thread
+     */
+    MAIN,
+
+
+    /**
+     * new thread
+     */
+    NEW_THREAD
+}
